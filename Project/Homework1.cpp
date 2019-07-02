6541bbcd94483753f28a6f3aa416d8b499d096cb
Supagin25130 <52159977+Supagin25130@users.noreply.github.com>
2019-07-02 23:04:54 +0700

    Homework1
    คำสั่ง cout

---
+++ Project/Homework1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main()
+{
+	string Name = "Supagin Chengkhao";
+	short Scores1 = 99;
+	short Scores2 = 80;
+	short Total = 179;
+	char Grade = 'B';
+	float gpa = 2.89;
+
+	cout << "My name is : " << Name << endl;
+	cout << "My scores 1 : " << Scores1 << endl;
+	cout << "My scores 2 : " << Scores2 << endl;
+	cout << "Total : " << Total << endl;
+	cout << "Grade : " << Grade << endl;
+	cout << "Gpa : " << gpa << endl;
+
+	return(0);
+}
